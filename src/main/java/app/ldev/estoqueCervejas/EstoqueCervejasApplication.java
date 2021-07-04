92bd53acfd787379a859dea52adac5e3bde1cd80
Leandro <leandro_csilva@ymail.com>
2021-07-04 00:37:22 -0300

    Gerado projeto com spring initializr com depêndencias

---
+++ src/main/java/app/ldev/estoqueCervejas/EstoqueCervejasApplication.java
@@ -0,0 +1,13 @@
+package app.ldev.estoqueCervejas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoqueCervejasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstoqueCervejasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/ldev/estoqueCervejas/EstoqueCervejasApplicationTests.java
@@ -0,0 +1,13 @@
+package app.ldev.estoqueCervejas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoqueCervejasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
